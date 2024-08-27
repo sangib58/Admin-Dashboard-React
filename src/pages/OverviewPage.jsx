402025b7e24f513e1ca6e8d3668c3ab82171ebbc
sangib58 <sangibruse@gmail.com>
2024-08-28 00:30:11 +0600

    first commit

---
+++ src/pages/OverviewPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OverviewPage = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default OverviewPage
---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductsPage = () => {
+  return (
+    <div>Products</div>
+  )
+}
+
+export default ProductsPage
